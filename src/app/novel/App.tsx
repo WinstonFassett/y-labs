b6193f536adc09dc3d6c69050aa8b74b16f4037b
Winston Fassett <winston.fassett@gmail.com>
2024-03-31 13:42:30 -0500

    move globals

--- src/app/novel/App.tsx
+++ src/app/novel/App.tsx
@@ -2,7 +2,6 @@ import { RouterProvider } from "react-router-dom";
 import { AppGlobals } from "../../globals";
 import { router } from "./router";
 import "./styles/prosemirror.css";
-import "./styles/globals.css";
 
 type Props = {
   frontmatter: {
