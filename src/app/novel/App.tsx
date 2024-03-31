c32b2b392114f46c35f181acabdb8305777a6aee
Winston Fassett <winston.fassett@gmail.com>
2024-03-31 08:53:45 -0500

    fix: novel css globals

--- src/app/novel/App.tsx
+++ src/app/novel/App.tsx
@@ -2,6 +2,7 @@ import { RouterProvider } from "react-router-dom";
 import { AppGlobals } from "../../globals";
 import { router } from "./router";
 import "./styles/prosemirror.css";
+import "./styles/globals.css";
 
 type Props = {
   frontmatter: {
