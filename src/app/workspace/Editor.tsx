79abc211929661e019eeb0d52b35cb8d4a135e5e
Winston Fassett <winston.fassett@gmail.com>
2024-11-02 20:03:17 -0500

    cleanup

--- src/app/workspace/Editor.tsx
+++ src/app/workspace/Editor.tsx
@@ -1,13 +1,11 @@
 import { cn } from "@/lib/utils";
-import "@tldraw/tldraw/tldraw.css";
 import AppBar from "../shared/AppBar";
-import Codemirror from "./Codemirror";
 
 export function Editor({ className }: { className?: string }) {
   return (
     <div className={cn("flex flex-col", className)}>
       <AppBar />
-      <Codemirror className="flex-1" />
+      <div>Editor goes here</div>
     </div>
   );
 }
