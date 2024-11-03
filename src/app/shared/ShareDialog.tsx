3b5151d84770a0c2c0792f99f955ea7db75fe945
Winston Fassett <winston.fassett@gmail.com>
2024-11-03 10:09:21 -0600

    restore and refactor

--- src/app/shared/ShareDialog.tsx
+++ src/app/shared/ShareDialog.tsx
@@ -93,6 +93,26 @@ export function ShareDialog() {
     setValue("encrypt", encrypt);
   }, [encrypt]);
   watch();
+  const submit = handleSubmit((data) => {
+    if ($config) {
+      $config.set({
+        ...currentConfig,
+        ...data,
+        enabled: true,
+      } as any);
+    } else {
+      const { roomId, password } = data;
+      navigate(
+        `/edit/${docId}?roomId=${roomId}${password ? `&encrypt&x=${password}` : ""}`,
+      );
+    }
+  });
+  const stopSharing = (e) => {
+    e.preventDefault();
+    $config?.setKey("enabled", false);
+    $collabRoom?.disconnect();
+    navigate(`/edit/${docId}`);
+  };
   return (
     <>
       <Button
@@ -115,8 +135,189 @@ export function ShareDialog() {
         }}
       >
         <DialogContent>
-          <AdvancedSharingDialog onClose={close} />
+          {/* {(onClose) => ( */}
+          <form onSubmit={submit} noValidate>
+            <>
+              <DialogHeader className="flex flex-col gap-1">
+                {actionLabel}
+              </DialogHeader>
+              <div className="gap-6">
+                {
+                  <>
+                    {!isSharing && (
+                      <Controller
+                        name="roomId"
+                        defaultValue={
+                          roomId ?? latestDocRoom.get()[docId!] ?? generateId()
+                        }
+                        rules={{ required: true }}
+                        control={control}
+                        render={({ field }) => (
+                          <div>
+                            <Input
+                              {...field}
+                              value={field.value || ""}
+                              isDisabled={isSharing}
+                              isRequired
+                              isInvalid={!!errors.roomId}
+                              errorMessage={
+                                errors.roomId && "Room name is required"
+                              }
+                              label="Room"
+                              placeholder="Enter a room name to connect to"
+                              variant="bordered"
+                            />
+                          </div>
+                        )}
+                      ></Controller>
+                    )}
+                    <Controller
+                      name="encrypt"
+                      defaultValue={encrypt ?? false}
+                      control={control}
+                      render={({ field }) => (
+                        <Switch
+                          isSelected={field.value}
+                          isDisabled={isSharing}
+                          onValueChange={(v) => {
+                            setValue("encrypt", v);
+                          }}
+                        >
+                          Encrypt communication
+                        </Switch>
+                      )}
+                    ></Controller>
+                    {!isSharing && getValues().encrypt && (
+                      <Controller
+                        name="password"
+                        defaultValue={password || generateId()}
+                        rules={{ required: true }}
+                        control={control}
+                        render={({ field }) => (
+                          <div>
+                            <PasswordInput
+                              {...field}
+                              className="text-default-400"
+                              value={field.value || ""}
+                              isDisabled={isSharing}
+                              isInvalid={!!errors.password}
+                              isRequired
+                              endContent={
+                                <KeyRoundIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+                              }
+                              label="Password"
+                              placeholder="Enter a password to enable encryption"
+                              variant="bordered"
+                            />
+                          </div>
+                        )}
+                      ></Controller>
+                    )}
+                  </>
+                }
+                {!!isSharing && (
+                  <>
+                    <div>
+                      <p className="my-2">Sharing Link:</p>
+                      <div className="">
+                        <Snippet
+                          size="lg"
+                          symbol=""
+                          variant="flat"
+                          color="primary"
+                          classNames={{
+                            base: "text-xs max-w-full",
+                            pre: "overflow-hidden",
+                          }}
+                        >
+                          {[
+                            window.location.protocol,
+                            "//",
+                            window.location.host,
+                            window.location.pathname,
+                            "#/edit/",
+                            docId,
+                            "?roomId=",
+                            roomId,
+                            password ? `&x=${password}` : "",
+                          ].join("")}
+                        </Snippet>
+                      </div>
+                    </div>
+                    <div>
+                      <div>
+                        {(awarenessUsers?.size ?? 1) - 1} peers connected
+                      </div>
+                      {!!awarenessUsers &&
+                        Array.from(awarenessUsers).map(
+                          ([peerId, awareness]) => {
+                            console.log("awareness", peerId, awareness);
+                            const data = awareness.presence ?? awareness.user;
+                            if (!data) {
+                              return (
+                                <div key={peerId}>
+                                  Missing data {peerId}: {JSON.stringify(data)}
+                                </div>
+                              );
+                            }
+                            const { userName, color } = data;
+
+                            const isYou = peerId === awarenessClientID;
+                            if (isYou) return null;
+                            return (
+                              <div key={peerId}>
+                                <User
+                                  className="py-4"
+                                  key={peerId}
+                                  name={
+                                    userName ? (
+                                      <UserName {...{ userName, color }} />
+                                    ) : (
+                                      peerId
+                                    )
+                                  }
+                                  description={
+                                    isYou
+                                      ? "YOU"
+                                      : userName
+                                        ? undefined
+                                        : JSON.stringify(awareness)
+                                    //"Anonymous"
+                                  }
+                                  avatarProps={{
+                                    // src: `https://i.pravatar.cc/150?u=${peerId}`,
+                                    src: `https://avatar.vercel.sh/${userName}?size=32`,
+                                  }}
+                                />
+                              </div>
+                            );
+                          },
+                        )}
+                    </div>
+                  </>
+                )}
+              </div>
+              <DialogFooter>
+                <Button variant="light" onClick={close}>
+                  Close
+                </Button>
+                {isSharing ? (
+                  <Button color="danger" onClick={stopSharing}>
+                    Stop Sharing
+                  </Button>
+                ) : (
+                  <Button color="primary" type="submit">
+                    Share
+                  </Button>
+                )}
+              </DialogFooter>
+            </>
+          </form>
+          {/* )} */}
         </DialogContent>
+        {/* <DialogContent>
+          <AdvancedSharingDialog onClose={close} />
+        </DialogContent> */}
       </Dialog>
     </>
   );
@@ -124,212 +325,7 @@ export function ShareDialog() {
 
 // function OrigDialogContent () {
 //   return (
-//     <DialogContent>
-//     {/* {(onClose) => ( */}
-//     <form
-//       onSubmit={handleSubmit((data) => {
-//         if ($config) {
-//           $config.set({
-//             ...currentConfig,
-//             ...data,
-//             enabled: true,
-//           } as any);
-//         } else {
-//           const { roomId, password } = data;
-//           navigate(
-//             `/edit/${docId}?roomId=${roomId}${password ? `&encrypt&x=${password}` : ""}`,
-//           );
-//         }
-//         // // onClose();
-//       })}
-//       noValidate
-//     >
-//       <>
-//         <DialogHeader className="flex flex-col gap-1">
-//           {actionLabel}
-//         </DialogHeader>
-//         <div className="gap-6">
-//           {
-//             <>
-//               {!isSharing && (
-//                 <Controller
-//                   name="roomId"
-//                   defaultValue={
-//                     roomId ?? latestDocRoom.get()[docId!] ?? generateId()
-//                   }
-//                   rules={{ required: true }}
-//                   control={control}
-//                   render={({ field }) => (
-//                     <div>
-//                       <Input
-//                         {...field}
-//                         value={field.value || ""}
-//                         isDisabled={isSharing}
-//                         isRequired
-//                         isInvalid={!!errors.roomId}
-//                         errorMessage={
-//                           errors.roomId && "Room name is required"
-//                         }
-//                         label="Room"
-//                         placeholder="Enter a room name to connect to"
-//                         variant="bordered"
-//                       />
-//                     </div>
-//                   )}
-//                 ></Controller>
-//               )}
-//               <Controller
-//                 name="encrypt"
-//                 defaultValue={encrypt ?? false}
-//                 control={control}
-//                 render={({ field }) => (
-//                   <Switch
-//                     isSelected={field.value}
-//                     isDisabled={isSharing}
-//                     onValueChange={(v) => {
-//                       setValue("encrypt", v);
-//                     }}
-//                   >
-//                     Encrypt communication
-//                   </Switch>
-//                 )}
-//               ></Controller>
-//               {!isSharing && getValues().encrypt && (
-//                 <Controller
-//                   name="password"
-//                   defaultValue={password || generateId()}
-//                   rules={{ required: true }}
-//                   control={control}
-//                   render={({ field }) => (
-//                     <div>
-//                       <PasswordInput
-//                         {...field}
-//                         className="text-default-400"
-//                         value={field.value || ""}
-//                         isDisabled={isSharing}
-//                         isInvalid={!!errors.password}
-//                         isRequired
-//                         endContent={
-//                           <KeyRoundIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
-//                         }
-//                         label="Password"
-//                         placeholder="Enter a password to enable encryption"
-//                         variant="bordered"
-//                       />
-//                     </div>
-//                   )}
-//                 ></Controller>
-//               )}
-//             </>
-//           }
-//           {!!isSharing && (
-//             <>
-//               <div>
-//                 <p className="my-2">Sharing Link:</p>
-//                 <div className="">
-//                   <Snippet
-//                     size="lg"
-//                     symbol=""
-//                     variant="flat"
-//                     color="primary"
-//                     classNames={{
-//                       base: "text-xs max-w-full",
-//                       pre: "overflow-hidden",
-//                     }}
-//                   >
-//                     {[
-//                       window.location.protocol,
-//                       "//",
-//                       window.location.host,
-//                       window.location.pathname,
-//                       "#/edit/",
-//                       docId,
-//                       "?roomId=",
-//                       roomId,
-//                       password ? `&x=${password}` : "",
-//                     ].join("")}
-//                   </Snippet>
-//                 </div>
-//               </div>
-//               <div>
-//                 <div>
-//                   {(awarenessUsers?.size ?? 1) - 1} peers connected
-//                 </div>
-//                 {!!awarenessUsers &&
-//                   Array.from(awarenessUsers).map(
-//                     ([peerId, awareness]) => {
-//                       console.log("awareness", peerId, awareness);
-//                       const data = awareness.presence ?? awareness.user;
-//                       if (!data) {
-//                         return (
-//                           <div key={peerId}>
-//                             Missing data {peerId}: {JSON.stringify(data)}
-//                           </div>
-//                         );
-//                       }
-//                       const { userName, color } = data;
 
-//                       const isYou = peerId === awarenessClientID;
-//                       if (isYou) return null;
-//                       return (
-//                         <div key={peerId}>
-//                           <User
-//                             className="py-4"
-//                             key={peerId}
-//                             name={
-//                               userName ? (
-//                                 <UserName {...{ userName, color }} />
-//                               ) : (
-//                                 peerId
-//                               )
-//                             }
-//                             description={
-//                               isYou
-//                                 ? "YOU"
-//                                 : userName
-//                                   ? undefined
-//                                   : JSON.stringify(awareness)
-//                               //"Anonymous"
-//                             }
-//                             avatarProps={{
-//                               // src: `https://i.pravatar.cc/150?u=${peerId}`,
-//                               src: `https://avatar.vercel.sh/${userName}?size=32`,
-//                             }}
-//                           />
-//                         </div>
-//                       );
-//                     },
-//                   )}
-//               </div>
-//             </>
-//           )}
-//         </div>
-//         <DialogFooter>
-//           <Button variant="light" onClick={close}>
-//             Close
-//           </Button>
-//           {isSharing ? (
-//             <Button
-//               color="danger"
-//               onClick={(e) => {
-//                 e.preventDefault();
-//                 $config?.setKey("enabled", false);
-//                 $collabRoom?.disconnect();
-//                 navigate(`/edit/${docId}`);
-//               }}
-//             >
-//               Stop Sharing
-//             </Button>
-//           ) : (
-//             <Button color="primary" type="submit">
-//               Share
-//             </Button>
-//           )}
-//         </DialogFooter>
-//       </>
-//     </form>
-//     {/* )} */}
-//   </DialogContent>
 //   )
 // }
 
