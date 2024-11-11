81c81a6735d64ca95be10e3e10fb48d9f2c5d1a5
Winston Fassett <winston.fassett@gmail.com>
2024-11-11 17:16:24 -0600

    update menu

--- src/app/drive/DriveActions.tsx
+++ src/app/drive/DriveActions.tsx
@@ -1,11 +1,6 @@
+import { AriaButton } from "@/components/ui/aria-button";
+import { Menu, MenuItem, MenuPopover, MenuSeparator, MenuTrigger } from "@/components/ui/aria-menu";
 import { useTheme } from "@/lib/astro-tailwind-themes/useTheme";
-import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import {
   DownloadIcon,
   FileText,
@@ -15,88 +10,51 @@ import {
   UploadIcon,
 } from "lucide-react";
 import {
-  CreateDocumentDialogButton,
-  createDocumentState,
+  createDocumentState
 } from "./CreateDocumentDialogButton";
 import { doExport, doExportJson } from "./ExportDriveButton";
 import { startImport } from "./ImportDrive";
 
 export function DriveActions() {
-  return (
-    <div className="flex items-center">
-      <MoreMenu />
-    </div>
-  );
-}
-
-function MoreMenu() {
   const [theme, setTheme] = useTheme();
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger>
+    <MenuTrigger>
+      <AriaButton aria-label="Menu" size="icon" variant="outline" className="rounded-full">
         <MoreVertical size={20} />
-        {/* <Button variant="light" isIconOnly>
-        </Button> */}
-      </DropdownMenuTrigger>
-      <DropdownMenuContent
-        aria-label="More Actions"
-        color="default"
-        selectionMode="none"
-      >
-        <DropdownMenuItem
-          onClick={() => {
+      </AriaButton>
+      <MenuPopover>
+        <Menu>
+          <MenuItem onAction={() => {
             createDocumentState.open();
             console.log("opened");
-          }}
-          endContent={<FileText size={16} />}
-        >
-          New Document
-        </DropdownMenuItem>
-        <DropdownMenuItem>
-          <Divider />
-        </DropdownMenuItem>
-        <DropdownMenuItem
-          key="import"
-          onClick={startImport}
-          endContent={<UploadIcon size={16} />}
-        >
-          Import Drive
-        </DropdownMenuItem>
-        <DropdownMenuItem
-          key="export"
-          onClick={doExport}
-          endContent={<DownloadIcon size={16} />}
-        >
-          Export Drive
-        </DropdownMenuItem>
-        <DropdownMenuItem
-          key="export-json"
-          onClick={doExportJson}
-          endContent={<DownloadIcon size={16} />}
-        >
-          Export Drive to JSON
-        </DropdownMenuItem>
-        <DropdownMenuItem>
-          <Divider />
-        </DropdownMenuItem>
-        <DropdownMenuItem
-          key="toggleTheme"
-          endContent={
-            theme === "light" ? <Sun size={16} /> : <Moon size={16} />
-          }
-          closeOnSelect={false}
-          onClick={(e) => {
-            console.log("click");
-            setTheme(theme === "dark" ? "light" : "dark");
-          }}
-        >
-          Change Theme
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  );
-}
-
-function Divider() {
-  return <div className="border-t border-default-200 my-2" />;
+          }}>
+            <FileText size={16} />
+            New document
+          </MenuItem>
+          <MenuSeparator />
+          <MenuItem onAction={startImport}>
+            <UploadIcon size={16} />
+            Import Drive
+          </MenuItem>
+          <MenuItem onAction={doExport}>
+            <DownloadIcon size={16} />
+            Export Drive
+          </MenuItem>
+          <MenuItem onAction={doExportJson}>
+            <DownloadIcon size={16} />
+            Export Drive to JSON
+          </MenuItem>
+          <MenuSeparator />
+          <MenuItem
+            onAction={() => {
+              setTheme(theme === "dark" ? "light" : "dark");
+            }}
+          >
+            {theme === "light" ? <Sun size={16} /> : <Moon size={16} />}
+            Change Theme
+          </MenuItem>
+        </Menu>
+      </MenuPopover>
+    </MenuTrigger>
+  )
 }
