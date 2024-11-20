b0b3115ce245e6c191e3d2b055d48ffc462f9ea9
Winston Fassett <winston.fassett@gmail.com>
2024-11-20 17:32:54 -0600

    simplify common editor routes

--- src/app/tldraw/EditorRoute.tsx
+++ src/app/tldraw/EditorRoute.tsx
@@ -1,65 +1,8 @@
-import { useEffect } from "react";
-import { useNavigate, useParams, useSearchParams } from "react-router-dom";
 
-import { generateId } from "../shared/generateId";
-import { roomKeys } from "../shared/store/doc-room-keys";
+import { useEditorRoute } from "../shared/useEditorRoute";
 import Editor from "./Tldraw";
-import { getDocRoomConfig, roomConfigsByDocId } from "../shared/store/doc-room-config";
 
 export function EditorRoute() {
-  const params = useParams();
-  let { docId } = params;
-
-  const navigate = useNavigate();
-  const [searchParams, setSearchParams] = useSearchParams();
-  const x = searchParams.get("x");
-  const roomId = searchParams.get("roomId");
-  const encrypt = searchParams.has("encrypt");
-
-  // when docId, roomId or x changes, update password and encrypted
-  useEffect(() => {
-
-    if (roomId) {
-      const config = getDocRoomConfig(docId!, roomId!);
-      if (x) {
-        const newSearchParams = Object.fromEntries(
-          Array.from(searchParams.entries()).filter(([key]) => key !== "x"),
-        );
-        newSearchParams.encrypt = 'true'
-        const isNewDoc = !docId;
-        if (isNewDoc) {
-          docId = generateId();
-        }
-        config.set({ 
-          ...config.get(), 
-          docId: docId,
-          roomId: roomId,
-          password: x,
-          encrypt: true,
-          enabled: true,
-          accessLevel: "edit",
-        });
-        roomConfigsByDocId.setKey(docId!, config);
-        if (isNewDoc) {
-          navigate(`/edit/${docId}?roomId=${roomId}&encrypt=true"}`, {
-            replace: true,
-          });
-        } else {
-          setSearchParams(newSearchParams, { replace: true });
-        }
-        return
-      }
-      config.set({ 
-        ...config.get(), 
-        docId: docId,
-        roomId: roomId,
-        // password: "",
-        encrypt: false,
-        enabled: true,
-        accessLevel: "edit",
-      });
-    }
-  }, [docId, roomId, x]);
-
+  useEditorRoute()
   return <Editor className="h-screen" />;
 }
