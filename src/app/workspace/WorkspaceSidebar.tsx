23fedfbdb2f1b2ade0c4cdf56a457d8a82c2846c
Winston Fassett <winston.fassett@gmail.com>
2024-11-23 17:28:50 -0600

    cleanup and filter

--- src/app/workspace/WorkspaceSidebar.tsx
+++ src/app/workspace/WorkspaceSidebar.tsx
@@ -1,6 +1,7 @@
-import * as React from "react"
 import { ChevronRight, File, Folder, HardDriveIcon } from "lucide-react"
+import * as React from "react"
 
+import { documentsStore } from "@/app/drive/store"
 import {
   Collapsible,
   CollapsibleContent,
@@ -13,13 +14,11 @@ import {
   SidebarGroupContent,
   SidebarGroupLabel,
   SidebarMenu,
-  SidebarMenuBadge,
   SidebarMenuButton,
   SidebarMenuItem,
   SidebarMenuSub,
-  SidebarRail,
+  SidebarRail
 } from "@/components/ui/sidebar"
-import { documentsStore } from "@/app/drive/store"
 import { useStore } from "@nanostores/react"
 import { Button } from "../../components/ui/button"
 
@@ -27,52 +26,9 @@ function getDocUrl(name: string, type: string) {
   return `/y-labs/app/workspace/index.html#/edit/${name}`;  
 }
 
-// This is sample data.
-const data = {
-  changes: [
-    {
-      file: "README.md",
-      state: "M",
-    },
-    {
-      file: "api/hello/route.ts",
-      state: "U",
-    },
-    {
-      file: "app/layout.tsx",
-      state: "M",
-    },
-  ],
-  tree: [
-    // [
-    //   "app",
-    //   [
-    //     "api",
-    //     ["hello", ["route.ts"]],
-    //     "page.tsx",
-    //     "layout.tsx",
-    //     ["blog", ["page.tsx"]],
-    //   ],
-    // ],
-    // [
-    //   "components",
-    //   ["ui", "button.tsx", "card.tsx"],
-    //   "header.tsx",
-    //   "footer.tsx",
-    // ],
-    // ["lib", ["util.ts"]],
-    // ["public", "favicon.ico", "vercel.svg"],
-    // ".eslintrc.json",
-    // ".gitignore",
-    // "next.config.js",
-    // "tailwind.config.js",
-    // "package.json",
-    // "README.md",
-  ],
-}
-
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
-  const documents = useStore(documentsStore);
+  const allDocuments = useStore(documentsStore);
+  const documents = allDocuments?.filter(doc => doc.type === 'novel');
   if (!documents) {
     return <div>Loading...</div>;
   }
@@ -82,22 +38,6 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
     <Sidebar {...props}>
       <SidebarContent>
-        {/* <SidebarGroup>
-          <SidebarGroupLabel>Changes</SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {data.changes.map((item, index) => (
-                <SidebarMenuItem key={index}>
-                  <SidebarMenuButton>
-                    <File />
-                    {item.file}
-                  </SidebarMenuButton>
-                  <SidebarMenuBadge>{item.state}</SidebarMenuBadge>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup> */}
         <SidebarGroup className="items-start">
           <Button
             asChild
