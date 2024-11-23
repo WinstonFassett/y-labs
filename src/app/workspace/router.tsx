aec7922dbeda3a7a0d94be5f5ba43d2f88597b1e
Winston Fassett <winston.fassett@gmail.com>
2024-11-23 13:05:09 -0600

    route tweak

--- src/app/workspace/router.tsx
+++ src/app/workspace/router.tsx
@@ -1,6 +1,8 @@
 import { Navigate, createHashRouter } from "react-router-dom";
 import { EditorRoute } from "./EditorRoute";
 import { generateId } from "../shared/generateId";
+import { createHashRouter } from "react-router-dom";
+import { Workspace } from "./Workspace";
 
 export const routes = [
   {
