1876a198af80a91c91e97cbfa281a9d73575006c
Winston Fassett <winston.fassett@gmail.com>
2024-11-23 01:35:32 -0600

    Redo both sw paths for GH

--- src/lib/pwa/register-service-worker.ts
+++ src/lib/pwa/register-service-worker.ts
@@ -1,7 +1,7 @@
 console.log("service worker!");
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    .register("./service-worker.js")
+    .register("/y-labs/service-worker.js")
     .then((registration) => {
       console.log("Service worker successfully registered", registration.scope);
     })
