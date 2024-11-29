8aa51ca19ae7d8c7c2eb4c6689ae13a8e1fef4f0
Winston Fassett <winston.fassett@gmail.com>
2024-11-29 17:24:53 -0600

    fix codemirror

--- src/app/codemirror/Codemirror.tsx
+++ src/app/codemirror/Codemirror.tsx
@@ -16,11 +16,12 @@ import { atom } from "nanostores";
 function Codemirror({ className = "" }: { className?: string }) {
   const editor = useRef<HTMLDivElement>(null);
   const [theme] = useTheme();
-  const { docId, roomId, $room, ydoc } = useDocCollabStore();
-  useStore($room || atom({}));
-  const providerMaybe = $room?.get().provider;
-  const { $offline } = getDocLoadState(docId, roomId);
-  const loaded = useStore($offline.$loaded);
+
+  const { docId, ydoc, $room, roomId, needsPasswordToConnect } = useDocCollabStore();
+  const loadState = useStore(getDocLoadState(docId, roomId));
+  const fragment = ydoc.getXmlFragment("novel");
+  const provider = $room?.get().provider;
+  const loaded = loadState === "loaded";
 
   useEffect(
     function setupCodemirror() {
@@ -40,8 +41,8 @@ function Codemirror({ className = "" }: { className?: string }) {
 
       const themeExtensions =
         theme === "dark" ? [oneDark, customBackgroundExtension] : [];
-      const collabExtensions = providerMaybe
-        ? yCollab(ytext, providerMaybe.awareness)
+      const collabExtensions = provider
+        ? yCollab(ytext, provider.awareness)
         : [];
       const startState = EditorState.create({
         doc: ytext.toString(),
@@ -62,7 +63,7 @@ function Codemirror({ className = "" }: { className?: string }) {
         view.destroy();
       };
     },
-    [theme, providerMaybe, loaded],
+    [theme, provider, loaded],
   );
   return <div ref={editor} className={className}></div>;
 }
--- src/app/codemirror/Editor.tsx
+++ src/app/codemirror/Editor.tsx
@@ -2,12 +2,28 @@ import { cn } from "@/lib/utils";
 import "@tldraw/tldraw/tldraw.css";
 import AppBar from "../shared/AppBar";
 import Codemirror from "./Codemirror";
+import { PasswordRequiredDialog } from "../shared/PasswordRequiredDialog";
+import { useDocCollabStore } from "../shared/useDocCollabStore";
+import { getDocLoadState } from "../shared/store/doc-loader";
+import { useStore } from "@nanostores/react";
+import { useStoreIfPresent } from "../shared/useStoreIfPresent";
 
 export function Editor({ className }: { className?: string }) {
+  const { docId, roomId, needsPasswordToConnect, $roomConfig } = useDocCollabStore()
+  const $loader = getDocLoadState(docId!, roomId!);
+  const loadState = useStore($loader);
+  console.log("loadState", loadState);
+  const config = useStoreIfPresent($roomConfig);
+  
+  const loadingEncrypted = config && config.enabled && config.encrypt && loadState === "loading";
+  const canShow = (!needsPasswordToConnect || loadState === "loaded") && !loadingEncrypted;
+  // codemirror needs to rerender if password present but not loaded
   return (
     <div className={cn("flex flex-col", className)}>
       <AppBar />
-      <Codemirror className="flex-1" />
+      {!canShow && <div>Loading...</div>}
+      {canShow && <Codemirror className="flex-1" />}
+      <PasswordRequiredDialog />
     </div>
   );
 }
