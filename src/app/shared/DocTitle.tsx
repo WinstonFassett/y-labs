99b1d487affd926d473eb8a3ad70116fdd845f41
Winston Fassett <winston.fassett@gmail.com>
2024-11-30 16:44:48 -0600

    doc title autofocus

--- src/app/shared/DocTitle.tsx
+++ src/app/shared/DocTitle.tsx
@@ -8,17 +8,23 @@ import {
 import { useDebouncedCallback } from "use-debounce";
 import type { YMapEvent } from "yjs";
 import { useDocCollabStore } from "./useDocCollabStore";
+import { getDocLoadState } from "./store/doc-loader";
+import { useStore } from "@nanostores/react";
 
 const initialTitle = document.title;
 const setWindowTitle = (title: string) =>
   (document.title = `${title || "Untitled"} - ${initialTitle}`);
 
 export function DocTitle() {
-  const { ydoc } = useDocCollabStore();
+  const { ydoc, docId, roomId } = useDocCollabStore();
   const meta = ydoc.getMap<any>("meta");
   const title = (meta.get("title") as string) || "";
   const [pendingTitle, setPendingTitle] = useState(title);
-
+  useEffect(() => {
+    return () => {
+      onChangeDebounced.cancel();
+    }
+  },[])
   // update document.title when title changes
   useEffect(() => {
     setWindowTitle(title);
@@ -43,22 +49,28 @@ export function DocTitle() {
   };
   const onChangeDebounced = useDebouncedCallback(updateTitle, 1000);
   const hasTitle = title.length > 0;
+  const loadState = useStore(getDocLoadState(docId!, roomId!));
+  const isLoaded = loadState === "loaded";
   return (
-    <>
-      <Input
-        autoFocus={!hasTitle && ydoc.isLoaded}
-        placeholder={"Add a Title"}
-        value={pendingTitle}
-        // size="sm"
-        className="text-lg text-center bg-transparent border-transparent hover:border-border"
-        onBlur={(e) => {
-          updateTitle(e as ChangeEvent<HTMLInputElement>);
-        }}
-        onChange={(e) => {
-          setPendingTitle(e.target.value);
-          onChangeDebounced(e);
-        }}
-      />
+    <>{
+      !isLoaded ? <div></div> : (
+        <Input
+          key={docId}
+          autoFocus={!hasTitle}
+          placeholder={"Add a Title"}
+          value={pendingTitle}
+          // size="sm"
+          className="text-lg text-center bg-transparent border-transparent hover:border-border"
+          onBlur={(e) => {
+            updateTitle(e as ChangeEvent<HTMLInputElement>);
+          }}
+          onChange={(e) => {
+            setPendingTitle(e.target.value);
+            onChangeDebounced(e);
+          }}
+        />        
+      )
+    }
     </>
   );
 }
