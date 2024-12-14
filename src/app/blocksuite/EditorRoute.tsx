abd0dfa68542116f9d0b2ac6cd02fbd4d7856ca2
Winston Fassett <winston.fassett@gmail.com>
2024-12-14 14:55:15 -0600

    app bar

--- src/app/blocksuite/EditorRoute.tsx
+++ src/app/blocksuite/EditorRoute.tsx
@@ -1,8 +1,12 @@
 
+import AppBar from "../shared/AppBar";
 import { useDocRoomRoute } from "../shared/useDocRoomRoute";
 import BlocksuiteEditor from './BlocksuiteEditor'
 
 export function EditorRoute() {
   useDocRoomRoute()
-  return <BlocksuiteEditor />;
+  return <>
+    <AppBar />
+    <BlocksuiteEditor />;
+  </>
 }
