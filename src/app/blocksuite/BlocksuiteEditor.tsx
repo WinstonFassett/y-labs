5ad3e1903e14ce2adbafe04e8f0e6a82000022a1
Winston Fassett <winston.fassett@gmail.com>
2024-12-15 10:33:46 -0600

    styles

--- src/app/blocksuite/BlocksuiteEditor.tsx
+++ src/app/blocksuite/BlocksuiteEditor.tsx
@@ -9,7 +9,7 @@ import { useDocParams } from '../shared/useDocParams';
 import EditorContainer from './EditorContainer';
 import { EditorProvider } from './EditorProvider';
 import { createEditor } from './createEditor';
-import './index.css';
+import './style.css';
 import { Button } from '@/components/ui/button';
 
 function App() {
