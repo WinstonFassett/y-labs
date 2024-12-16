661d002552c62a673deeb6d3ee12f3528f5482fb
Winston Fassett <winston.fassett@gmail.com>
2024-12-16 07:59:15 -0600

    app bar, style

--- src/app/milkdown/EditorRoute.tsx
+++ src/app/milkdown/EditorRoute.tsx
@@ -1,8 +1,12 @@
 
+import AppBar from "../shared/AppBar";
 import { useDocRoomRoute } from "../shared/useDocRoomRoute";
 import Editor from "./MilkdownEditor";
 
 export function EditorRoute() {
   useDocRoomRoute()
-  return <Editor />;
+  return <div className="flex-1 flex flex-col">
+    <AppBar />
+    <Editor />    
+  </div>;
 }
