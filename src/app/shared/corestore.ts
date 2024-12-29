dd427e212ae17c8d4ebb3e9cb83802d0f7722cf9
Winston Fassett <winston.fassett@gmail.com>
2024-12-29 09:57:00 -0600

    corestore

--- src/app/shared/corestore.ts
+++ src/app/shared/corestore.ts
@@ -7,9 +7,4 @@ import { createFile } from '@zacharygriffee/random-access-idb';
 export const keyPair = crypto.keyPair();
 export const keystr = b4a.toString(keyPair.publicKey, 'hex');
 
-// const createFile = RAI('corestore')
-
-export const corestore = new Corestore((filename: string) => {
-  console.log(`Creating storage for ${filename}`);
-  return createFile(filename, { chunkSize: 1024 });  
-});
+export const corestore = new Corestore(createFile);
